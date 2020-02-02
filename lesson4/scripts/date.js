32daa6a17957915cfc473e94253a14893c56dd0e
Meglegs <meglegsss@gmail.com>
2020-02-01 18:26:11 -0700

    Preston City

---
+++ lesson4/scripts/date.js
@@ -0,0 +1,31 @@
+
+let dayNames = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday"
+];
+
+let monthNames = [
+    "January",
+    "February",
+    "March",
+    "April",
+    "May",
+    "June",
+    "July",
+    "August",
+    "September",
+    "October",
+    "November",
+    "December"
+];
+
+let d = new Date();
+let dayName = dayNames[d.getDay()];
+let monthName = monthNames[d.getMonth()];
+let fullDate = dayName + ", " + d.getDate() + " " + monthName + " " + d.getFullYear();
+document.getElementById("date").innerHTML = fullDate;
